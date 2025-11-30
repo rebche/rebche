a533a8abc00f4ca466e0d5c1e846369ca7fdb34c
rebche <chen.rebecca@northeastern.edu>
2025-11-30 13:59:48 -0500

    spent more time on snackbar to tell ppl that the dark mode doesnt work than actually work on dark mode LOL

--- js/main.js
+++ js/main.js
@@ -49,3 +49,11 @@ window.onclick = function(event) {
         });
     }
 }
+
+function snackbar() {
+    var x = document.getElementById("snackbar");
+
+    x.className = "show";
+
+    setTimeout(function(){x.className = x.className.replace("show", "");}, 3000);
+}
